f534ae57c07153566543b1ce0fb68f8f2f841589
AMISHAHI <110057532+Amisha-05@users.noreply.github.com>
2023-04-23 11:38:20 +0530

    Add files via upload

---
+++ Python/ALL PANDAS.py
@@ -0,0 +1,42 @@
+# pandas as pd
+#import numpy as np 
+#df=pd.DataFrame({'X':(1,2,3),'Y':(4,5,6),'Z':(7,8,'5')})
+#print(df)
+
+#pandas using index labelled!
+#import pandas as pd
+#import numpy as np
+#exam_data={'Name':['Amisha','Diya','Nidhi','Raju','Tisha'],
+  #         'Score':[50,55,56,57,54],
+    #       'Attempts':[1,3,np.nan,6,7]}
+#labels=['a','b','c','d','e']
+#df=pd.DataFrame(exam_data,index=labels)
+#print(df)
+
+#import pandas as pd
+ # import numpy as np
+#exam_data={'Name':['Amisha','Diya','Nidhi','Raju','Tisha'],
+        #   'Score':[50,55,56,57,54],
+       #   'Attempts':[1,3,np.nan,6,7]}
+#labels=['a','b','c','d','e']
+#df=pd.DataFrame(exam_data,index=labels)
+#print("first three rows are:")
+#print(df.iloc[3:])
+
+
+
+#import pandas as pd
+#import numpy as np
+#exam_data={'Name':['Amisha','Diya','Nidhi','Raju','Tisha'],
+      #  'Score':[30,55,36,57,54],
+        #'Attempts':[1,3,np.nan,6,7]}
+#labels=['a','b','c','d','e']
+#df=pd.DataFrame(exam_data,index=labels)
+#print("ROWS WHOSE SCORE IS GREATER THAN 50 IS:")
+#print(df[df['Score'] > 50])
+
+
+import pandas as pd
+import numpy as np
+data1={'key1':K0,K0,K1,K2}
+
